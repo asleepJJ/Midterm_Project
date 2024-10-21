4bd608718f473f1ac0b8893c9bfb70945e539b13
asleepJJ <jeraldjaybuban16@gmail.com>
2024-10-21 19:39:39 +0800

    Updated Code

--- resources/views/lab3.blade.php
+++ resources/views/lab3.blade.php
@@ -16,9 +16,13 @@
         </p>
         <h3>SUMMARY</h3>
         <p>
-        "Welcome to my portfolio, this is the compilation of the hands-on laboratory activities and its documentation. 
-        Each laboratory activity shows my understanding and what I learned so far, 
-        demonstrating my journey through various technical challenges and solutions."
+        In Laboratory 3, I learned how to create a layout file using Blade, which serves as a template for other views. 
+        Instead of writing the same HTML structure repeatedly, I now use a layout file to define the general structure of the website, 
+        such as headers, footers, and navigation bars. This saved me a lot of time and made the code cleaner.
+        I applied Blade directives such as "@ extends", "@ section", and "@ yield" to let specific pages inherit the layout and insert unique content, making the site consistent and efficient. 
+        I also used other Blade features like “@ csrf” to protect forms from cross-site request forgery (CSRF) attacks. 
+        By setting up routes in “web.php” to serve these views and learned how to handle route-to-view connections effectively, 
+        I streamlined the development process and made the code cleaner and more organized for this lab.
         </p>
         <a href="{{ asset('documents/BUBAN_JeraldJay_lab3.pdf') }}" target="_blank">View Document File</a>
         <br>
--- resources/views/lab4.blade.php
+++ resources/views/lab4.blade.php
@@ -16,9 +16,13 @@
         </p>
         <h3>SUMMARY</h3>
         <p>
-        "Welcome to my portfolio, this is the compilation of the hands-on laboratory activities and its documentation. 
-        Each laboratory activity shows my understanding and what I learned so far, 
-        demonstrating my journey through various technical challenges and solutions."
+        In Laboratory 4, I learned how to create and implement middleware in Laravel, which acts as a filter for HTTP requests. 
+        Middleware can inspect or modify requests before they reach the application. 
+        I used this knowledge to create a middleware that checks if a user is of the correct age (e.g., 18 or older) that directs 
+        and gives the user access to the pages, and if the user is below 18, the user is redirected to an "Access Denied" page 
+        and can’t access the remaining pages. And also, if the user inputs an age above 21, there will be a page that will prompt to the user 
+        saying that it’s the “Restricted Area” but still, they can access the remaining pages. Understanding how to register and apply middleware 
+        to specific routes helped me manage access control and improve security in the web application for my Laboratory 4.
         </p>
         <a href="{{ asset('documents/BUBAN_JeraldJay_lab4.pdf') }}" target="_blank">View Document File</a>
         <br>
